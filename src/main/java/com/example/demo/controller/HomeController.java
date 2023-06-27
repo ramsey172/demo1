651184de590ff08f6db5727093c09338e2897c49
ramsey17 <benksyff@gmail.com>
2023-06-27 20:50:40 +0300

    commit

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,2 +1,18 @@
-package com.example.demo.controller;public class HomeController {
+package com.example.demo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping
+    public String home(Model model){
+        model.addAttribute("test","test17");
+        return "home";
+    }
+
 }
