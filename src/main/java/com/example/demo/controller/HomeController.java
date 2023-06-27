265d8119c612bd5d4652f2bb0c75cab2765b631b
ramsey17 <benksyff@gmail.com>
2023-06-27 21:10:03 +0300

    commit

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @GetMapping
     public String home(Model model){
-        model.addAttribute("test","test127");
+        model.addAttribute("test","t2est12e7");
         return "home";
     }
 
